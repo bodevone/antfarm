068e6923bdc85b74d48db12e6fbcd5327fcafe91
Alibi Shalgymbay <aliby.bbb@gmail.com>
2020-04-07 13:50:04 +0600

    Remove decryption in travis

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+set -x
+
+openssl aes-256-cbc -K $encrypted_dfdcfd5172af_key -iv $encrypted_dfdcfd5172af_iv -in deploy_key.enc -out ./deploy_key -d
 eval "$(ssh-agent -s)"
 chmod 600 ./deploy_key
 echo -e "Host $SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
