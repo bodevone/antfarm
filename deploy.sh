df43e39597003875066abd762dc4e5f2c2eea192
Alibi Shalgymbay <aliby.bbb@gmail.com>
2020-04-07 13:07:15 +0600

    Add deploy script to travis

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+eval "$(ssh-agent -s)"
+chmod 600 ./deploy_key
+echo -e "Host $SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+ssh-add ./deploy_key
+ssh -i ./deploy_key root@$SERVER_IP_ADDRESS <<EOF
+    ls
+EOF
