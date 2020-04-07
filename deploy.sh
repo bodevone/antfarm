1b0e675c132e3fcf9624c455d8d7c49a91b23911
Alibi Shalgymbay <aliby.bbb@gmail.com>
2020-04-07 14:09:45 +0600

    Add script for rebuilding containers to travis

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,9 @@ chmod 600 ./deploy_key
 echo -e "Host $SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add ./deploy_key
 ssh -i ./deploy_key root@$SERVER_IP_ADDRESS <<EOF
-    ls
+    cd antfarm
+    git pull
+    docker-compose down
+    docker-compose build --no-cache
+    docker-compose up -d
 EOF
